ce1f41b9bf213c0dc86a059848242c17eacbaba4
Sumeet Subhash Nikam <sumeetrikam0088@gmail.com>
2026-02-03 21:53:21 +0530

    Make the socket.js productuion ready b removing localhost calls

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,11 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const SOCKET_URL =
+  import.meta.env.MODE === "production"
+    ? import.meta.env.VITE_SOCKET_URL
+    : "http://localhost:5000";
+
+const socket = io(SOCKET_URL, {
   transports: ["websocket"],
 });
 
